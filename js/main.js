1e3f57b1efacdd827b2721dba87666b422b95a3f
Varkh <vark.den@gmail.com>
2015-03-08 00:55:52 +0200

    Add comments to model; Style fix for tab body.

--- js/main.js
+++ js/main.js
@@ -10,5 +10,32 @@ var tabInfo = {
     title: "Вона табулатура",
     band: "Плач Єремії",
     postedDate: "August 24, 2013 at 9:00 PM",
-    tabInfo: "(Завтра прийде до кімнати) \nCлова: Кость Москалець \nМузика: Кость Москалець \nОбробка слів, переклад: Mishanya solo version, найкращий підбір"
+    otherInfo: [
+        "(Завтра прийде до кімнати)",
+        "Cлова: Кость Москалець",
+        "Музика: Кость Москалець",
+        "Обробка слів, переклад: Mishanya solo version",
+        "найкращий підбір"
+    ],
+    body: "",
+    comments: [
+        {
+            title: "Start Bootstrap",
+            postedDate: "August 23, 2014 at 9:30 PM",
+            text: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.",
+            subcomments: []
+        },
+        {
+            title: "Start Bootstrap",
+            postedDate: "August 25, 2014 at 9:30 PM",
+            text: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.",
+            subcomments: [
+                {
+                    title: "Nested Start Bootstrap",
+                    postedDate: "August 26, 2014 at 9:30 PM",
+                    text: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.",
+                }
+            ]
+        }
+    ]
 };
