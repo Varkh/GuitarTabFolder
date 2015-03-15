029c2028407a008e62bacef931cedcf7aa97926d
Varkh <vark.den@gmail.com>
2015-03-16 00:38:59 +0200

    Move tabPage data to server and get as AJAX

--- public/js/main.js
+++ public/js/main.js
@@ -2,40 +2,9 @@
 
 var app = angular.module('tabPage', []);
 
-app.controller('TabulaturController', function(){
-    this.tabInfo = tabInfo;
-});
-
-var tabInfo = {
-    title: "Вона табулатура",
-    band: "Плач Єремії",
-    postedDate: "August 24, 2013 at 9:00 PM",
-    otherInfo: [
-        "(Завтра прийде до кімнати)",
-        "Cлова: Кость Москалець",
-        "Музика: Кость Москалець",
-        "Обробка слів, переклад: Mishanya solo version",
-        "найкращий підбір"
-    ],
-    body: "",
-    comments: [
-        {
-            title: "Start Bootstrap",
-            postedDate: "August 23, 2014 at 9:30 PM",
-            text: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.",
-            subcomments: []
-        },
-        {
-            title: "Start Bootstrap",
-            postedDate: "August 25, 2014 at 9:30 PM",
-            text: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.",
-            subcomments: [
-                {
-                    title: "Nested Start Bootstrap",
-                    postedDate: "August 26, 2014 at 9:30 PM",
-                    text: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.",
-                }
-            ]
-        }
-    ]
-};
+app.controller('TabulaturController', [ '$scope', '$http', function($scope, $http) {
+    $http.get("/tabContent?id=1")
+        .success(function (response) {
+            $scope.tabInfo = response;
+        });
+}]);
