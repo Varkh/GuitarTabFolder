17aca4625a83c4c9fcc6ffa8d7f379479f07d370
Vark <vark.den@gmail.com>
2015-03-16 19:06:45 +0200

    Reinit app with Express template

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,25 @@
 var express = require('express');
+var router = express.Router();
 
-module.exports = function(app) {
-    app.use(express.static('public'));
+/* GET home page. */
+router.get('/', function(request, response, next) {
+  //res.render('index', { title: 'Express' });
+    response.redirect(301, './pages/about.html');
+});
 
-    app.get('/', function(request, response) {
-        response.redirect(301, './pages/about.html');
-    });
+module.exports = router;
+/*
+ app.use(express.static('public'));
 
-    app.get('/getTabList', function(request, response) {
-        var blocks = ['aaa', 'bbb', 'ccc'];
-        response.json(blocks);
-    });
+ app.get('/getTabList', function(request, response) {
+ var blocks = ['aaa', 'bbb', 'ccc'];
+ response.json(blocks);
+ });
 
-    app.get('/tabContent', function(request, response) {
-        response.json(tabs[request.query.id]);
-    });
-};
+ app.get('/tabContent', function(request, response) {
+ response.json(tabs[request.query.id]);
+ });
+ */
 
 var tabs = {
     1: {
